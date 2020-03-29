87820260a8f55c746da93072917986b52b7f4c2e
Isaque de Souza <isaquesb@gmail.com>
2020-03-29 16:42:35 -0300

    Fix resource, add email!

--- src/resource/ong.js
+++ src/resource/ong.js
@@ -3,6 +3,7 @@ const cb = (data) => {
         id: data.id,
         name: data.name,
         cellphone: data.cellphone,
+        email: data.email,
         city: data.city,
         uf: data.uf,
         created_at: data.created_at ? new Date(data.created_at) : undefined
